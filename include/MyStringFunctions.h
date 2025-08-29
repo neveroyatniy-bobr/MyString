16ec45012ee7089058771901e96517bc82b31bf5
neveroyatniy-bobr <nicant2007@yandex.ru>
2025-08-29 18:13:20 +0300

    Добавил doxygen коментарии и создал иерархию проэкта

---
+++ include/MyStringFunctions.h
@@ -0,0 +1,10 @@
+#ifndef MY_STRING_FUNCTIONS_H_
+#define MY_STRING_FUNCTIONS_H_
+
+/// @brief Ищет первое появление символа в строке
+/// @param str строка
+/// @param ch символ
+/// @return указатель на символ в строке
+const char* MyStrChr(const char* str, int ch);
+
+#endif // MY_STRING_FUNCTIONS_H_
