4bf76579816948e46e980f652f6cd0c5a2f161ef
neveroyatniy-bobr <nicant2007@ygmail.com>
2025-09-11 15:39:47 +0300

    Добавил doxygen коментарий для RectLinePointer()

--- include/My2DArrayFunctions.h
+++ include/My2DArrayFunctions.h
@@ -3,6 +3,11 @@
 
 #include <stdio.h>
 
+/// @brief Вычисляет указатель на начало строки в 2мерном прямоугольном массиве масивве
+/// @param line_num номер строки
+/// @param line_size максимальная длина строки
+/// @param array2d 2мерный массив
+/// @return Указатель на начало строки
 int* RectLinePointer(size_t line_num, size_t line_size, int* array2d);
 
 /// @brief Выводит 2мерный массив в поток стандартного вывода
